a9c93ba4bfc04bdb9b53b6de5e4b01843784d25c
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-10-22 07:37:10 +0530

    Complete the Hand class of the game

--- GameOfCards/src/main/java/GameOfCards/Game/SaatPeSaatGame/SaatHand.java
+++ GameOfCards/src/main/java/GameOfCards/Game/SaatPeSaatGame/SaatHand.java
@@ -1,6 +1,7 @@
 package GameOfCards.Game.SaatPeSaatGame;
 
 import java.util.List;
+import java.util.Scanner;
 import java.io.Serializable;
 import java.util.ArrayList;
 import GameOfCards.Basics.*;
@@ -8,6 +9,9 @@ import GameOfCards.Basics.*;
 public class SaatHand extends Hand implements Serializable {
     private static final long serialVersionUID = 1;
     private Card cardOnTop;
+    private boolean gameStatus;
+    private boolean winner;
+    private int action;
 
     public SaatHand() {
         super();
@@ -17,7 +21,11 @@ public class SaatHand extends Hand implements Serializable {
         cardOnTop = card;
     }
 
-    public String evaluateOptions() {
+    public Card getCardOnTop() {
+        return cardOnTop;
+    }
+
+    public List<Integer> evaluateOptions() {
         Rank rankValue = cardOnTop.getRank();
         Suit suitValue = cardOnTop.getSuit();
         List<Integer> possible = new ArrayList<Integer>();
@@ -33,6 +41,43 @@ public class SaatHand extends Hand implements Serializable {
                 }
             }
         }
-        return "Possible plays: " + possible.toString();
+        return possible;
+    }
+
+    public boolean isGameRunning() {
+        return gameStatus;
+    }
+
+    public void setGameStatus(boolean status) {
+        gameStatus = status;
+    }
+
+    public void setWinner(boolean winner) {
+        this.winner = winner;
+    }
+
+    public boolean isWinner() {
+        return winner;
+    }
+
+    public void setAction() {
+        List<Integer> opt = evaluateOptions();
+        System.out.println("Your turn!");
+        if (opt.size() == 0) {
+            System.out.println("No possible plays");
+            action = -1;
+            return;
+        }
+        System.out.println("Possible plays: " + opt.toString());
+        System.out.print("Select an option: ");
+        Scanner input = new Scanner(System.in);
+        action = input.nextInt();
+        input.close();
+        if (opt.indexOf(action) < 0)
+            action = opt.get(0);
+    }
+
+    public int getAction() {
+        return action;
     }
 }
