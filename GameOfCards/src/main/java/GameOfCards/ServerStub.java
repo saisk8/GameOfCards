8b5f307fcfd4efa3361ae001a31209a6eed30cf7
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-10-29 23:47:45 +0530

    Feature: Make the Server a generic one

--- GameOfCards/src/main/java/GameOfCards/ServerStub.java
+++ GameOfCards/src/main/java/GameOfCards/ServerStub.java
@@ -2,58 +2,55 @@ package GameOfCards;
 
 import java.net.*;
 import java.io.*;
-import GameOfCards.Game.SaatPeSaatGame.*;
+import GameOfCards.Basics.Acquaintance;
+import GameOfCards.Basics.Comms;
+import GameOfCards.Basics.GameServer;
 
 public class ServerStub extends Thread {
-
-    Socket theConnection;
-
-    public ServerStub(Socket s) {
-        theConnection = s;
-    }
+    private final static int PORT = 3000;
+    static int numberOfGroups = 0;
+    static int numberOfPlayersExpected = -1;
 
     public static void main(String[] args) {
-
-        int thePort;
         ServerSocket listeningSocket;
-
-        // set the port to listen on
         try {
-            thePort = Integer.parseInt(args[0]);
-            if (thePort < 0 || thePort > 65535)
-                thePort = 3000;
-        } catch (Exception e) {
-            thePort = 3000;
-        }
-
-        try {
-            int index = 0;
-            Socket sockets[] = new Socket[3];
-            listeningSocket = new ServerSocket(thePort);
+            listeningSocket = new ServerSocket(PORT);
             System.out.println("Accepting connections on port " + listeningSocket.getLocalPort());
             while (true) {
-                sockets[index++] = listeningSocket.accept();
-                if (index == 3) {
-                    break;
-                }
+                Socket newClient = listeningSocket.accept();
+                Acquaintance guest =
+                        Comms.receiveHello(new ObjectInputStream(newClient.getInputStream()));
+                Acquaintance welcome = greetings(guest);
+                int game = guest.getOption();
+                assignDealer(game, guest);
+                Comms.sendWelcome(welcome, new ObjectOutputStream(newClient.getOutputStream()));
             }
-            SaatPeSaat sp = new SaatPeSaat(sockets, 3);
-            sp.init();
-            sp.startGame();
         } catch (IOException e) {
             System.err.println("Server aborted prematurely");
         }
     }
 
-    public static void sendMess(Socket[] sockets) {
-        for (int i = 0; i < 3; i++) {
-            try {
-                DataOutputStream outToClient = new DataOutputStream(sockets[i].getOutputStream());
-                String toSend = "String to send\n";
-                outToClient.writeBytes(toSend);
-            } catch (IOException e) {
-                System.out.println("Error\n");
-            }
+    public static Acquaintance greetings(Acquaintance guest) {
+        Acquaintance welcome = new Acquaintance();
+        if (guest.isHost()) {
+            welcome.setStatus(1);
+            return welcome;
         }
+        welcome.setStatus(-1);
+        return welcome;
+    }
+
+    public static void assignDealer(int gameId, Acquaintance guest) {
+        numberOfGroups++;
+        GameServer game = null;
+        switch (gameId) {
+            case 1:
+                game = new GameServer(numberOfGroups, guest.getNumberOfPlayers(), gameId);
+            case 2:
+                game = new GameServer(numberOfGroups, guest.getNumberOfPlayers(), gameId);
+            default:
+                game = new GameServer(numberOfGroups, guest.getNumberOfPlayers(), 1);
+        }
+        game.run();
     }
 }
