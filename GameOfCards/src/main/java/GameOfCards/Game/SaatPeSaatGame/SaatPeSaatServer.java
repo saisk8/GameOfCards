5cec738f7fc373af4f6a13649f6820a5ad859d6d
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-10-30 19:15:04 +0530

    Feature: Add debug info

--- GameOfCards/src/main/java/GameOfCards/Game/SaatPeSaatGame/SaatPeSaatServer.java
+++ GameOfCards/src/main/java/GameOfCards/Game/SaatPeSaatGame/SaatPeSaatServer.java
@@ -10,22 +10,26 @@ public class SaatPeSaatServer {
     static int numberOfPlayers;
 
     public SaatPeSaatServer(int groupId, int number) {
-        thePort += PORT + groupId;
+        thePort = PORT + groupId;
         numberOfPlayers = number;
     }
 
     public void init() {
         ServerSocket listeningSocket;
         try {
+            System.out.println("Port: " + thePort + " Number of Players: " + numberOfPlayers);
             int index = 0;
             Socket sockets[] = new Socket[numberOfPlayers];
             listeningSocket = new ServerSocket(thePort);
-            System.out.println("Accepting connections on port " + listeningSocket.getLocalPort());
+            System.out.println("Accepting connections on port " + listeningSocket.getLocalPort()
+                    + " IP = " + listeningSocket.getInetAddress().toString());
             while (true) {
+                System.out.println("Again1 " + listeningSocket.getLocalPort());
                 sockets[index++] = listeningSocket.accept();
                 System.out.println("Number of players expected at " + listeningSocket.getLocalPort()
                         + " is/are: " + (numberOfPlayers - index));
                 if (index == numberOfPlayers) {
+                    // System.out.println("Not breaking");
                     break;
                 }
             }
@@ -33,7 +37,7 @@ public class SaatPeSaatServer {
             sps.init();
             sps.startGame();
         } catch (IOException e) {
-            System.err.println("Server aborted prematurely");
+            System.err.println("Server aborted prematurely " + thePort);
         }
     }
 }
